b49feee34bec7eb040bb3c4941f1c2d77b17e845
xu.miche@gmail.com <xu.miche@gmail.com>
2017-07-06 10:57:40 -0700

    Installed body parser and used it to convert JSON strings to JavaScript objects. #1

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 const notes = []
 
 app.get('/', (req, res) => {
   res.json(notes)
-  console.log(notes)
 })
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
 })
+
+app.use(bodyParser.json())
