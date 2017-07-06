48a4ef7dc8514988523b08a343437f7a2835b43b
xu.miche@gmail.com <xu.miche@gmail.com>
2017-07-06 11:21:49 -0700

    Registered a route for POST requests. #1

--- index.js
+++ index.js
@@ -8,14 +8,13 @@ app.get('/', (req, res) => {
   res.json(notes)
 })
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000')
-})
-
 app.use(bodyParser.json())
 
 app.post('/', (req, res) => {
-  console.log(req.body)
   notes.push(req.body)
   res.sendStatus(201)
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
 })
