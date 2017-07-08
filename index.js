675c877d295ff68293b3b3080cee806b67916ddb
xu.miche@gmail.com <xu.miche@gmail.com>
2017-07-08 13:19:26 -0700

    Registered a route for DELETE requests. #4

--- index.js
+++ index.js
@@ -29,6 +29,18 @@ app.put('/notes/:id', (req, res) => {
   res.sendStatus(200)
 })
 
+app.delete('/notes/:id', (req, res) => {
+  const noteId = Number(req.params.id)
+  const index = notes.findIndex((note) => {
+    return note.id === noteId
+  })
+  if (index === -1) {
+    return res.sendStatus(404)
+  }
+  notes.splice(index, 1)
+  res.sendStatus(204)
+})
+
 app.listen(3000, () => {
   console.log('Listening on port 3000')
 })
